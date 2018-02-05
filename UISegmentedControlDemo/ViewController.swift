c1ed72bcf9289423e9b9b3ab2209e8a0eecc0588
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2018-02-05 15:35:03 +0200

    UISegmentedControl Demo finished

--- UISegmentedControlDemo/ViewController.swift
+++ UISegmentedControlDemo/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    var mySegments: [String] = ["Red","Green","Blue"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        segmentController.removeAllSegments()
+        
+        for i in 0..<mySegments.count {
+            segmentController.insertSegment(withTitle: mySegments[i], at: i, animated: true)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onSegmentTapped(_ sender: UISegmentedControl) {
+        colorLabel.textColor = UIColor.white
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.backgroundColor = UIColor.red
+            colorLabel.text = sender.titleForSegment(at: 0)
+        case 1:
+            self.view.backgroundColor = UIColor.green
+            colorLabel.text = sender.titleForSegment(at: 1)
+        case 2:
+            self.view.backgroundColor = UIColor.blue
+            colorLabel.text = sender.titleForSegment(at: 2)
+        default:
+            self.view.backgroundColor = UIColor.gray
+        }
     }
-
-
 }
 
