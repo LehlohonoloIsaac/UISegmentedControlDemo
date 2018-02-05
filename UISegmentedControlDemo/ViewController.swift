f40fff23511eb606c5507b6ea3327efc8a1a3838
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2018-02-05 14:53:44 +0200

    Initial Commit

---
+++ UISegmentedControlDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISegmentedControlDemo
+//
+//  Created by Lehlohonolo Mbele on 2018/02/05.
+//  Copyright © 2018 Lehlohonolo Mbele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
